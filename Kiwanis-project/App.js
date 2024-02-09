293e59cba8bf59b11abf1db09c589c544c31fe72
CharlesDESC <charles.deschamps@ynov.com>
2024-02-09 19:16:07 +0100

    nav working

--- Kiwanis-project/App.js
+++ Kiwanis-project/App.js
@@ -3,15 +3,21 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { AuthStack } from "./src/navigation/AuthStack";
 import { WelcomeScreen } from "./src/screens/WelcomeScreen";
+import { Provider as PaperProvider } from "react-native-paper";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
 	return (
-		<NavigationContainer>
-			<Stack.Navigator>
-				<Stack.Screen name='Welcome' component={WelcomeScreen} />
-				<Stack.Screen name='AuthStack' component={AuthStack} />
-			</Stack.Navigator>
-		</NavigationContainer>
+		<PaperProvider>
+			<NavigationContainer>
+				<Stack.Navigator
+					initialRouteName='Welcome'
+					screenOptions={{ headerShown: false }}
+				>
+					<Stack.Screen name='Welcome' component={WelcomeScreen} />
+					<Stack.Screen name='AuthStack' component={AuthStack} />
+				</Stack.Navigator>
+			</NavigationContainer>
+		</PaperProvider>
 	);
 }
