2106702299acc0ae9cb25b890601427f6ce1f7e4
CharlesDESC <charles.deschamps@ynov.com>
2024-02-20 14:58:33 +0100

    modif nav if +15 ans

--- Kiwanis-project/src/screens/register/ValidChildScreen.jsx
+++ Kiwanis-project/src/screens/register/ValidChildScreen.jsx
@@ -2,25 +2,33 @@ import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
 export const ValidChildScreen = ({ route, navigation }) => {
-    const { userUid } = route.params;
+	const { userUid } = route.params;
 
-    return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Compte créé avec succès ! UID: {userUid}</Text>
-            {/* Ajoutez ici d'autres logiques ou boutons selon vos besoins */}
-            <Button title="Retour à l'accueil" onPress={() => navigation.popToTop()} />
-        </View>
-    );
+	return (
+		<View style={styles.container}>
+			<Text style={styles.text}>Compte créé avec succès ! UID: {userUid}</Text>
+			{/* Ajoutez ici d'autres logiques ou boutons selon vos besoins */}
+			<Button
+				title="Retour à l'accueil"
+				onPress={() => {
+					navigation.reset({
+						index: 0,
+						routes: [{ name: "Welcome" }],
+					});
+				}}
+			/>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    text: {
-        fontSize: 16,
-        marginBottom: 20,
-    },
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	text: {
+		fontSize: 16,
+		marginBottom: 20,
+	},
 });
